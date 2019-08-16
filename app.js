d3aff9037230ff86518bc4bd87bd1166d0e3d21b
Ralph Wiest <lioncrazed@gmail.com>
2019-08-16 13:33:17 -0400

    added all type notes

--- app.js
+++ app.js
@@ -1,2 +1,67 @@
 "use strict";
-console.log('It works!');
+// inferred types
+// string
+var myName = 'Max';
+// number
+var myAge = 27;
+// boolean
+var hasHobbies = false;
+// any
+var something;
+// explicit types
+// string
+var myRealName = 'Max';
+// number
+var myRealAge = 27;
+// boolean
+var hasRealHobbies = false;
+// array
+var hobbies = ['Cooking', 'Sports'];
+// tuples
+var address = ['Winding Way', 9];
+// enum
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["White"] = 1] = "White";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+var myColor = Color.Blue;
+console.log(myColor + ' is the enum value of 2');
+(function (Color) {
+    Color[Color["Orange"] = 100] = "Orange";
+    Color[Color["Yellow"] = 101] = "Yellow";
+    Color[Color["Green"] = 102] = "Green";
+})(Color || (Color = {}));
+var myNewColor = Color.Yellow;
+console.log(myNewColor + ' is the next enum value of 101');
+// any
+var car = 'BMW';
+console.log(car);
+car = { brand: 'BMW', series: 3 };
+console.log(car);
+// functions
+function returnMyName() {
+    return myName;
+}
+console.log(returnMyName());
+// void
+function sayHello() {
+    console.log('Hello!');
+}
+sayHello();
+// argument types
+function multiply(value1, value2) {
+    return value1 * value2;
+}
+console.log(multiply(2, 5));
+// function types
+var myMultiply;
+myMultiply = multiply;
+console.log(myMultiply(2, 5));
+//objects
+var userData = {
+    name: 'Max',
+    age: 27
+};
+console.log('My name is ' + userData.name);
--- app.ts
+++ app.ts
@@ -1,1 +1,121 @@
-console.log('It works!');
+// inferred types
+// string
+let myName = 'Max';
+// number
+let myAge = 27;
+// boolean
+let hasHobbies = false;
+// any
+let something;
+
+// explicit types
+// string
+let myRealName: string = 'Max';
+// number
+let myRealAge: number = 27;
+// boolean
+let hasRealHobbies: boolean = false;
+
+// array
+let hobbies: string[] = ['Cooking', 'Sports'];
+
+// tuples
+let address: [string, number] = ['Winding Way', 9];
+
+// enum
+enum Color {
+  Red,
+  White,
+  Blue
+}
+let myColor: Color = Color.Blue
+console.log(myColor + ' is the enum value of 2');
+
+enum Color {
+  Orange = 100,
+  Yellow,
+  Green
+}
+let myNewColor: Color = Color.Yellow
+console.log(myNewColor + ' is the next enum value of 101');
+
+// any
+let car: any = 'BMW';
+console.log(car);
+car = { brand: 'BMW', series: 3 };
+console.log(car);
+
+// functions
+function returnMyName(): string {
+  return myName;
+}
+console.log(returnMyName());
+
+// void
+function sayHello(): void {
+  console.log('Hello!');
+}
+sayHello();
+
+// argument types
+function multiply(value1: number, value2: number): number {
+  return value1 * value2;
+}
+console.log(multiply(2, 5));
+
+// function types
+let myMultiply: (a: number, b: number) => number;
+myMultiply = multiply;
+console.log(myMultiply(2, 5));
+
+// objects
+let userData: { name: string, age: number } = {
+  name: 'Max',
+  age: 27
+}
+console.log('My name is ' + userData.name);
+
+// complex object
+let complex: {data: number[], output: (all: boolean) => number[]} = {
+  data: [100, 3.99, 10],
+  output: function (all: boolean): number[] {
+    return this.data;
+  }
+}
+
+// type alias
+type Complex = {data: number[], output: (all: boolean) => number[]};
+let complex1: Complex = {
+  data: [100, 3.99, 10],
+  output: function (all: boolean): number[] {
+    return this.data;
+  }
+}
+let complex2: Complex = {
+  data: [100, 3.99, 10],
+  output: function (all: boolean): number[] {
+    return this.data;
+  }
+}
+
+// union types
+let myRealRealAge: number | string = 27;
+myRealRealAge = '27';
+
+// check types
+let finalValue = 30;
+if (typeof finalValue == 'number') {
+  console.log('finalValue is a number');
+}
+
+// never
+function neverReturns(): never {
+  throw new Error('An error!');
+}
+
+// nullable types - "strictNullChecks" must be set to true in tsconfig.json
+let cannotBeNull = 12;
+// cannotBeNull = null;    causes an error
+let canBeNull: number | null;
+canBeNull = 12;
+canBeNull = null;
